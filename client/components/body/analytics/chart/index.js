254b9d984ba3d5b9ce60edc4ab9b5d6e53553217
Netomation <netomation@gmail.com>
2018-05-19 22:00:29 +0300

    added a little style to analytic route

--- client/components/body/analytics/chart/index.js
+++ client/components/body/analytics/chart/index.js
@@ -164,54 +164,145 @@ export default class Chart extends React.Component{
      }
 
      pie = () => {
+
+        var colors = Highcharts.getOptions().colors,
+        categories = [
+          "Clicked",
+          "Not clicked",
+        ],
+        data = [
+          {
+            "y": 50,
+            "color": colors[2],
+            "drilldown": {
+              "name": "Chrome",
+              "categories": [
+                "compatibility",
+                "intrestingness",
+              ],
+              "data": [
+                80,
+                70
+              ]
+            }
+          },
+          {
+            "y":50,
+            "color": colors[1],
+            "drilldown": {
+              "name": "Firefox",
+              "categories": [
+                "compatibility",
+                "intrestingness",
+              ],
+              "data": [
+                80,
+                50
+              ]
+            }
+          },
+        ],
+        browserData = [],
+        versionsData = [],
+        i,
+        j,
+        dataLen = data.length,
+        drillDataLen,
+        brightness;
+      
+      
+      // Build the data arrays
+      for (i = 0; i < dataLen; i += 1) {
+      
+        // add browser data
+        browserData.push({
+          name: categories[i],
+          y: data[i].y,
+          color: data[i].color
+        });
+      
+        // add version data
+        drillDataLen = data[i].drilldown.data.length;
+        for (j = 0; j < drillDataLen; j += 1) {
+          brightness = 0.2 - (j / drillDataLen) / 5;
+          versionsData.push({
+            name: data[i].drilldown.categories[j],
+            y: data[i].drilldown.data[j],
+            color: Highcharts.Color(data[i].color).brighten(brightness).get()
+          });
+        }
+      }
+      
+          
         const values = this.props.categories
         console.log(values)
         var myChart = Highcharts.chart(this.props.id, {
             chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
                 type: 'pie',
                 width: this.props.width,
                 height: this.props.height
-            },
-            title: {
-                text: ''
-            },
-            // colors: this.props.color, //['#0052CC', '#0051ca99'],    
-              
-              tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+              },
+              title: {
+                text: 'Browser market share, January, 2018'
+              },
+              subtitle: {
+                text: 'Source: <a href="http://statcounter.com" target="_blank">statcounter.com</a>'
+              },
+              yAxis: {
+                title: {
+                  text: 'Total percent market share'
+                }
               },
               plotOptions: {
                 pie: {
-                  allowPointSelect: true,
-                  cursor: 'pointer',
-                  dataLabels: {
-                    enabled: true,
-                    format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                    style: {
-                      color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                    }
-                  }
+                  shadow: false,
+                  center: ['50%', '50%']
                 }
               },
+              tooltip: {
+                valueSuffix: '%'
+              },
               series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: [{
-                  name: 'Chrome',
-                  y: 80,
-                  sliced: true,
-                  selected: true
-                }, {
-                  name: 'Internet Explorer',
-                  y: 20
+                name: 'Browsers',
+                data: browserData,
+                size: '60%',
+                dataLabels: {
+                  formatter: function () {
+                    return this.y > 5 ? this.point.name : null;
+                  },
+                  color: '#ffffff',
+                  distance: -30
+                }
+              }, {
+                name: 'Versions',
+                data: versionsData,
+                size: '80%',
+                innerSize: '60%',
+                dataLabels: {
+                  formatter: function () {
+                    // display only if larger than 1
+                    return this.y > 1 ? '<b>' + this.point.name + ':</b> ' +
+                      this.y + '%' : null;
+                  }
                 },
-                ]
-              }]
-            });
-        }
+                id: 'versions'
+              }],
+              responsive: {
+                rules: [{
+                  condition: {
+                    maxWidth: 400
+                  },
+                  chartOptions: {
+                    series: [{
+                      id: 'versions',
+                      dataLabels: {
+                        enabled: false
+                      }
+                    }]
+                  }
+                }]
+              }
+            });}
   
      componentDidMount(){
         if(this.props.type=== 'line')
--- client/components/body/analytics/index.jsx
+++ client/components/body/analytics/index.jsx
@@ -3,6 +3,13 @@ import styles from './analytics.scss';
 import Slider from "react-slick";
 import Chart from './chart';
 
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+
+import faCertificate from '@fortawesome/fontawesome-free-solid/faCertificate'
+import faGem from '@fortawesome/fontawesome-free-solid/faGem'
+import faUserCircle from '@fortawesome/fontawesome-free-solid/faUserCircle'
+import faHeart from '@fortawesome/fontawesome-free-solid/faHeart'
+
 const data = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
 
 const Test = () => {
@@ -20,27 +27,58 @@ const Test = () => {
             <div className={styles.infoContainer} >
                 <div className={styles.compatibilityContainer}>
                     <div className={styles.header}>Compatibility</div>
-                    <div className={styles.info}></div>
+                    <div className={styles.info}>
+                        <FontAwesomeIcon className={styles.icon} icon={faCertificate} />        
+                        80%
+                    </div>
                 </div>
                 <div className={styles.intrestingnessContainer}>
                     <div className={styles.header}>Intrestingness</div>
-                    <div className={styles.info}></div>
+                    <div className={styles.info}>
+                        <FontAwesomeIcon className={styles.icon} icon={faGem} />        
+                    
+                        70%                    
+                    </div>
                 </div>
 
                 <div className={styles.quantityContainer}>
                     <div className={styles.header}>Quantity</div>
-                    <div className={styles.info}></div>
+                    <div className={styles.info}>
+                        <FontAwesomeIcon className={styles.icon} icon={faUserCircle} />        
+                    
+                        1000
+                    </div>
                 </div>
 
                 <div className={styles.clicksContainer}>
                     <div className={styles.header}>Clicks</div>
-                    <div className={styles.info}></div>
+                    <div className={styles.info}>
+                        <FontAwesomeIcon className={styles.icon} icon={faHeart} />        
+                        
+                        500
+                    </div>
                 </div>
                 <div className={styles.chart}>
                     <Chart 
                         id={'0'}
                         type={'pie'}
-                        height = {700}
+                        height = {650}
+                        series ={
+                            [{
+                                name: 'people',
+                                colorByPoint: true,
+                                data: [{
+                                  name: 'Not clicked',
+                                  y: 80,
+                                  sliced: true,
+                                  selected: true
+                                }, {
+                                  name: 'Clicked',
+                                  y: 20
+                                },
+                                ]
+                              }]
+                        }
                         // color= {['#0052CC', '#0051ca99']}
                     />
                 </div>
